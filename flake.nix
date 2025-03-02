ee44aaf94c134a0fd9cfabca37b21a7047d26e47
Qiming Chu <cchuqiming@gmail.com>
2025-03-03 00:28:12 +0800

    scripts: add nix flake

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    inputs@{
+      nixpkgs,
+      parts,
+      treefmt-nix,
+      ...
+    }:
+    parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        treefmt-nix.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              nixfmt.package = pkgs.nixfmt-rfc-style;
+              clang-format.enable = true;
+            };
+            flakeCheck = true;
+          };
+          packages = {
+            llvm_18 = pkgs.llvm_18;
+          };
+          devShells.default =
+            with pkgs;
+            mkShell {
+              inputsFrom = [
+                llvm_18
+              ];
+              buildInputs = [
+                gcc
+                llvm_18
+                cmake
+                ninja
+              ];
+            };
+        };
+    };
+}
