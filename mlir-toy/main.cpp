497ea8f83a8051da24cf7c3bba94c79b0cf8a04f
Qiming Chu <cchuqiming@gmail.com>
2025-03-03 17:54:31 +0800

    mlir-toy: add testcase and mlir generate

--- mlir-toy/main.cpp
+++ mlir-toy/main.cpp
@@ -1,23 +1,46 @@
 #include "mlir/IR/AsmState.h"
+#include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/Parser/Parser.h"
 #include "mlir/Support/FileUtilities.h"
-#include "mlir/Dialect/Func/IR/FuncOps.h"
+
 #include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace mlir;
 
-int main(int argc, char ** argv) {
+int main(int argc, char **argv) {
   MLIRContext ctx;
-  // First, we register the dialects we want to use
   ctx.loadDialect<func::FuncDialect, arith::ArithDialect>();
-  // Then, we parse the input file
-  auto src = parseSourceFile<ModuleOp>(argv[1], &ctx);
-  // Output the dialect to llvm::outs(), but we can also output to llvm::errs(), llvm::dbgs()
-  src->print(llvm::outs());
-  // Simple output, often used in debug
-  src->dump();
+
+  // Create OpBuilder
+  OpBuilder builder(&ctx);
+  auto mod = builder.create<ModuleOp>(builder.getUnknownLoc());
+
+  // Set insertion point
+  builder.setInsertionPointToEnd(mod.getBody());
+
+  // Create func
+  auto i32 = builder.getI32Type();
+  auto funcType = builder.getFunctionType({i32, i32}, {i32});
+  auto func =
+      builder.create<func::FuncOp>(builder.getUnknownLoc(), "test", funcType);
+
+  // Add entry block
+  auto entry = func.addEntryBlock();
+  auto args = entry->getArguments();
+
+  // Set insertion point
+  builder.setInsertionPointToEnd(entry);
+
+  // Create arith.addi
+  auto addi =
+      builder.create<arith::AddIOp>(builder.getUnknownLoc(), args[0], args[1]);
+
+  // Create func.return
+  builder.create<func::ReturnOp>(builder.getUnknownLoc(), ValueRange({addi}));
+  mod->print(llvm::outs());
   return 0;
 }
